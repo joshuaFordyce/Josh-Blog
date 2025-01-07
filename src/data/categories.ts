205c44d905d34365b940fe1d03f0d40d65eec5aa
joshua fordyce880 <jfordyce880@gmail.com>
2025-01-06 22:56:22 -0500

    Update categories.ts

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -1,7 +1,7 @@
 // List of categories for blog posts
 export const CATEGORIES = [
 	'Machine Learning',
-	'Data engineering and Data Infrastructure',
+	'Data Engineering and Data Infrastructure',
 	'Cloud Infrastructure',
 	'API Development',
 ] as const
